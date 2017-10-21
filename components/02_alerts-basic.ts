64ede382d6faccaea920124981dcbcf4adc56069
Prachya Phetananya <puy.prachya@gmail.com>
2017-10-21 09:00:59 +0700

    add alert - basic

---
+++ components/02_alerts-basic.ts
@@ -0,0 +1,15 @@
+import { AlertController } from 'ionic-angular';
+
+export class MyPage {
+  constructor(public alertCtrl: AlertController) {
+  }
+
+  showAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'New Friend!',
+      subTitle: 'Your friend, Obi wan Kenobi, just accepted your friend request!',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+}
